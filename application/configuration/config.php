1998ddb52a4f0aa515fa2b527b51d0873c10e918
Pablo Santamarta Esteban <pablosesteban@gmail.com>
2016-06-15 11:41:16 +0200

    Change configuration paths to run in wamp.

--- application/configuration/config.php
+++ application/configuration/config.php
@@ -4,7 +4,7 @@
  */
 
 define ('HOME', $_SERVER['DOCUMENT_ROOT']);
-define ('APPLICATION', HOME . '/application');
+define ('APPLICATION', HOME . '/../application');
 define ('CONFIGURATION', APPLICATION . '/configuration');
 define ('VIEWS', APPLICATION . '/views');
 define ('LAYOUTS', VIEWS . '/layouts');
@@ -12,13 +12,13 @@ define ('CONTROLLERS', APPLICATION . '/controllers');
 define ('MODELS', APPLICATION . '/models');
 
 
-define ('PUBLICA', HOME . "/public");
+define ('PUBLICA', HOME);
 define ('CSS', PUBLICA . '/css');
 define ('IMAGES', PUBLICA . '/img');
 define ('JAVASCRIPT', PUBLICA . '/js');
 
 
-define ('URLAPPLICATION', 'http://' . $_SERVER['HTTP_HOST'] . "/public");
+define ('URLAPPLICATION', 'http://' . $_SERVER['HTTP_HOST']);
 define ('URLCSS', URLAPPLICATION . '/css');
 define ('URLIMAGES', URLAPPLICATION . '/img');
 define ('URLJAVASCRIPT', URLAPPLICATION . '/js');
